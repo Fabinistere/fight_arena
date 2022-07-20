c8a429d69195f9bb896e60da0c82b1ebf23598ea
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-07-20 20:24:29 +0200

    new constants file with a bit of constants

---
+++ src/constants.rs
@@ -0,0 +1,23 @@
+pub const BACKGROUND_COLOR: bevy::render::color::Color = bevy::render::color::Color::Rgba {
+    red: 58.0 / 256.0,
+    green: 36.0 / 256.0,
+    blue: 48.0 / 256.0,
+    alpha: 1.0,
+};
+
+pub const CLEAR: bevy::render::color::Color = bevy::render::color::Color::rgb(0.1,0.1,0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
+
+pub mod locations {
+    pub mod temple {
+        pub const BACKGROUND_Z: f32 = 0.0;
+
+        pub const TEMPLE_Z: f32 = 2.0;
+
+        pub const THRONE_Z_BACK: f32 = 3.0;
+        pub const THRONE_Z_FRONT: f32 = 6.0;
+        pub const THRONE_POSITION: (f32, f32, f32) = (-230.0, 900.0, THRONE_Z_BACK);
+    }
+}
+
