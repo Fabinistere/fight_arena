24ebdfbea6dd35ec6229e0b621448deca79c1f68
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-08-03 18:41:27 +0200

    the npc now run to the direction and rest after
    huge meliboulga with the approximation of the position of the npc (+- 1)

--- src/npc.rs
+++ src/npc.rs
@@ -9,94 +9,169 @@ pub struct NPCPlugin;
 
 #[derive(Component)] // Inspectable
 pub struct NPC {
-    speed: f32
+    // check if we can remove this field and replace it by just get_label()
+    name: String,
+    speed: f32,
+    direction: Vec3,
+    state: NPCState
 }
 
+// State for individual entity ?!
+// without using the bevy state tool
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 enum NPCState {
     Running,
-    Following,
+    // Following,
     Rest,
-    Talking
+    // Talking
 }
 
 impl Plugin  for NPCPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_state(NPCState::Running)
             .add_startup_system(spawn_character)
-            .add_system(character_movement)
+            .insert_resource(RestTime {
+                timer: Timer::from_seconds(10.0, false)
+            })
+            .insert_resource(RunningTime {
+                timer: Timer::from_seconds(30.0, false)
+            })
+            .add_system(stroll)
+            .add_system(rest)
             ;
-
     }
 }
 
+
+/*
+ * NPC has hobbies
+ *  - landwark
+ *    - index in const, with free: bol
+ *    - when talking to a npc in a landwark, include the other present
+ *    -> rest
+ *  - stroll
+ *    - in a restricted zone -index in const-
+ *    -> rest
+ *  - rest
+ *    -> stroll
+ *    -> landwark
+ *  - talking to MC
+ *    - infite rest until the MC is leaving
+ *    -> short rest
+ *    or
+ *    -> stroll
+ *    -> landmark
+ *    -> rest
+ * 
+ * Reflexion
+ *  - should npc avoid hit other entity
+ *  - turn false the free param from a landmark position taken by the MC
+ */
+
+fn stroll(
+    mut npc_query: Query<(&mut NPC, &mut Transform)>,
+    time: Res<Time>,
+    mut npc_state: ResMut<State<NPCState>>,
+) {
+    for (mut npc, mut transform) in npc_query.iter_mut() {
+
+        // TODO remove this conditon, and find a better solution
+        if npc.state == NPCState::Running {
+
+            let direction = npc.direction;
+
+            if ((direction.y*100.0) as i32 != (transform.translation.y*100.0) as i32     &&
+                (direction.y*100.0) as i32 != (transform.translation.y*100.0) as i32 + 1 &&
+                (direction.y*100.0) as i32 != (transform.translation.y*100.0) as i32 - 1)
+                ||
+               ((direction.x*100.0) as i32 != (transform.translation.x*100.0) as i32     &&
+                (direction.x*100.0) as i32 != (transform.translation.x*100.0) as i32 + 1 &&
+                (direction.x*100.0) as i32 != (transform.translation.x*100.0) as i32 - 1)
+            {
+
+                if direction.y > transform.translation.y {
+                    transform.translation.y += npc.speed * TILE_SIZE * time.delta_seconds();
+                }
+            
+                if direction.y < transform.translation.y {
+                    transform.translation.y -= npc.speed * TILE_SIZE * time.delta_seconds();
+                }
+            
+                if direction.x > transform.translation.x {
+                    transform.translation.x += npc.speed * TILE_SIZE * time.delta_seconds();
+                }
+            
+                if direction.x < transform.translation.x {
+                    transform.translation.x -= npc.speed * TILE_SIZE * time.delta_seconds();
+                }
+            } else {
+                println!(
+                    "I'm {} and I'm gonna rest for a while",
+                    npc.name
+                );
+                npc.state = NPCState::Rest;
+            }
+
+            /*println!(
+                "direction: {}, {} pos: {}
+                \n pos+1: [{}, {}, {}]
+                \n pos-1: [{}, {}, {}]",
+                direction,
+                npc.name,
+                transform.translation,
+                (transform.translation.x*100.0) as i32 +1,
+                (transform.translation.y*100.0) as i32 +1,
+                transform.translation.z,
+                (transform.translation.x*100.0) as i32 -1,
+                (transform.translation.y*100.0) as i32 -1,
+                transform.translation.z,
+
+            );
+            */
+
+            /*
+             direction: [-1, 0.8, 0], Admiral pos: [-1.0008445, 0.79996014, 5]
+             direction: [0.3, -1, 0], Olf pos: [0.29992545, -1.0007071, 5]
+            */
+
+        }
+    }
+}
+
+fn rest(
+    time: Res<Time>,
+) {
+
+    // flexing animation
+
+
+
+}
+            
+
 #[derive(Component)]
-struct FuseTime {
+struct RestTime {
     /// track when the npc should stop rest (non-repeating timer)
     timer: Timer,
 }
 
-/// repos: timer
-fn character_movement(
-    mut npc_query: Query<(&mut NPC, &mut Transform)>,
-    time: Res<Time>,
-){
+#[derive(Component)]
+struct RunningTime {
+    /// whenever destination is reached or not
+    /// he will stop moving after this timer ends
+    /// track when the npc should stop run (non-repeating timer)
+    timer: Timer,
+}
 
-    // , &mut FuseTime)>
-    
-
-    // like my brain BIG but not so usefull (lie?)
-
-    let (npc, mut transform) = npc_query.single_mut();
-
-    let direction = give_a_direction();
-    println!("npc just got a way to go");
-    println!("x: {} y: {} z: {}", direction.x, direction.y, direction.z);
-    // destination.y
-    
-    if direction.y != transform.translation.y &&
-          direction.x != transform.translation.x {
-
-        if direction.y > transform.translation.y {
-            transform.translation.y += npc.speed * TILE_SIZE * time.delta_seconds();
-        }
-        
-        if direction.y < transform.translation.y {
-            transform.translation.y -= npc.speed * TILE_SIZE * time.delta_seconds();
-        }
-
-        if direction.x > transform.translation.x {
-            transform.translation.x += npc.speed * TILE_SIZE * time.delta_seconds();
-        }
-        
-        if direction.x < transform.translation.x {
-            transform.translation.x -= npc.speed * TILE_SIZE * time.delta_seconds();
-        }
-    }
-
-    // insert state: Rest
-
-    println!("npc just wanna rest");
-
-    // commands.entity(npc)
-    //         .insert(FuseTime {
-    //             // create the non-repeating fuse timer
-    //             timer: Timer::new(Duration::from_secs(5), false),
-    //         });
-
-    // for (_, _, mut fuse_timer) in npc_query.iter_mut() {
-    //     // timers gotta be ticked, to work
-    //     fuse_timer.timer.tick(time.delta());
-
-    //     if fuse_timer.timer.finished() {
-    //         // consider this as death
-    //         commands.entity(npc).despawn();
-    //         println!("npc just despawn");
-    //     }
-
-    // }
-
-    // https://bevy-cheatbook.github.io/features/time.html
-
+/// Configure our resting time algorithm
+fn setup_rest_time(
+    mut commands: Commands,
+) {
+    commands.insert_resource(RestTime {
+        // create the repeating timer
+        timer: Timer::new(Duration::from_secs(10), true),
+    })
 }
 
 /**
@@ -108,8 +183,8 @@ fn character_movement(
  */
 fn give_a_direction() -> Vec3
 {
-    let x = rand::thread_rng().gen_range(-10..10) as f32 / 100.0;
-    let y = rand::thread_rng().gen_range(-10..10) as f32 / 100.0;
+    let x = rand::thread_rng().gen_range(-10..10) as f32 / 10.0;
+    let y = rand::thread_rng().gen_range(-10..10) as f32 / 10.0;
     // let z = rand::thread_rng().gen_range(1..101);
 
     let direction = Vec3::new(x, y, 0.0);
@@ -123,17 +198,45 @@ fn spawn_character(
     fabien: Res<FabienSheet>
 )
 {
-    let character = spawn_fabien_sprite(
+    let position = Vec3::new(-0.2, 0.35, 5.0);
+
+    let admiral = spawn_fabien_sprite(
         &mut commands,
         &fabien,
         0,
         Color::rgb(0.9,0.9,0.9),
-        Vec3::new(-0.2, 0.35, 5.0),
+        position,
+        Vec3::new(2.0,2.0,0.0)
+    );
+
+    let olf = spawn_fabien_sprite(
+        &mut commands,
+        &fabien,
+        12,
+        Color::rgb(0.9,0.9,0.9),
+        Vec3::new(-0.2, 0.55, 5.0),
         Vec3::new(2.0,2.0,0.0)
     );
 
     commands
-        .entity(character)
-        .insert(Name::new("NPC"))
-        .insert(NPC { speed: 3.0 });
+        .entity(admiral)
+        .insert(Name::new("NPC Admiral"))
+        .insert(
+        NPC {
+            name: "Admiral".to_string(),
+            speed: 3.0,
+            direction: give_a_direction(),
+            state: NPCState::Running
+        });
+
+    commands
+        .entity(olf)
+        .insert(Name::new("NPC Olf"))
+        .insert(
+        NPC {
+            name: "Olf".to_string(),
+            speed: 3.0,
+            direction: give_a_direction(),
+            state: NPCState::Running
+        });
 }
