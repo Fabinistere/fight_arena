b1b8a5eb5fa93ff9f10c51452e7a1dbbdfd401e2
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-08-30 12:59:35 +0200

    lower the speed and add velocity to the movement bundle

--- src/movement.rs
+++ src/movement.rs
@@ -1,15 +1,17 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::Velocity;
 
 #[derive(Component, Deref, DerefMut)]
 pub struct Speed(pub f32);
 
 impl Default for Speed {
     fn default() -> Self {
-        Speed (3.0)
+        Speed (1.0)
     }
 }
 
 #[derive(Bundle)]
 pub struct MovementBundle {
     pub speed: Speed,
+    pub velocity: Velocity
 }
