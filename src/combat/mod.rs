2e32b879ad5424ebb6b79747796994218e65df05
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-09-20 23:19:47 +0200

    Leader and CombatState

--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -1,32 +1,57 @@
-use bevy::{prelude::*, ecs::schedule::ShouldRun};
+use bevy::{
+    ecs::schedule::ShouldRun,
+    time::FixedTimestep, 
+    prelude::*,
+};
 
 pub mod stats;
 
 use crate::{
-    GameState,
-    combat::stats::*
+    combat::stats::*,
+    constants::FIXED_TIME_STEP,
 };
 
-
+#[derive(PartialEq, Clone, Hash, Debug, Eq, SystemLabel)]
+enum CombatState {
+    Observation,
+    SetUpStuff,
+    SelectionSkills,
+    SelectionTarget,
+    RollInitiative,
+    ExecuteSkills,
+    // ShowExecution,
+}
 
 pub struct CombatPlugin;
 
 impl Plugin for CombatPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_system_set(
-                SystemSet::on_enter(GameState::Combat)
-                    .with_system(roll_initiative)
+            // .add_system_to_stage(
+            //     CoreStage::Update,
+            //     stats::roll_initiative
+            //         .with_run_criteria(FixedTimestep::step(FIXED_TIME_STEP as f64))
+            //         .label(CombatState::RollInitiative)
+            // )
+            .add_system_to_stage(
+                CoreStage::Update,
+                observation
+                    .with_run_criteria(FixedTimestep::step(FIXED_TIME_STEP as f64))
+                    .label(CombatState::Observation)
             )
-            .add_system_set_to_stage(
-                CoreStage::PostUpdate,
-                SystemSet::new()
-                    .with_run_criteria(run_if_pressed_h)
-                    .with_system(show_hp)
-                    .with_system(show_mana)
-            )
+            // .add_system_set_to_stage(
+            //     CoreStage::PostUpdate,
+            //     SystemSet::new()
+            //         .with_run_criteria(run_if_pressed_h)
+            //         .with_system(show_hp)
+            //         .with_system(show_mana)
+            // )
             ;
     }
+}
+
+fn observation(){
+    // println!("Now it's your turn...")
 }
 
 fn run_if_pressed_h(
@@ -38,6 +63,9 @@ fn run_if_pressed_h(
         ShouldRun::No
     }
 }
+
+#[derive(Clone, Copy, Component)]
+pub struct Leader;
 
 #[derive(Clone, Copy, Component)]
 pub struct Target(pub Option<Entity>);
