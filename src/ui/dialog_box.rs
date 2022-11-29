b746f02b5894e011e4a7d4930e93b9bbbc5a8715
Wabtey <samurai.banzai@laposte.net>
2022-11-29 13:21:20 +0100

    indentation facist classic move (4 -> 5)

--- src/ui/dialog_box.rs
+++ src/ui/dialog_box.rs
@@ -13,12 +13,12 @@ use bevy_tweening::{lens::UiPositionLens, *};
 use std::time::Duration;
 
 use crate::{
-    constants::ui::dialogs::*,
-    npc::{
-        aggression::{CombatEvent, CombatExitEvent},
-        NPC,
-    },
-    ui::dialog_system::{init_tree_flat, DialogType},
+     constants::ui::dialogs::*,
+     npc::{
+          aggression::{CombatEvent, CombatExitEvent},
+          NPC,
+     },
+     ui::dialog_system::{init_tree_flat, DialogType},
 };
 
 use super::dialog_system::Dialog;
@@ -29,29 +29,29 @@ pub struct DialogPanel;
 /// TODO add entity id (u32) into the DialogBox
 #[derive(Debug, Component)]
 pub struct DialogBox {
-    text: String,
-    progress: usize,
-    finished: bool,
-    update_timer: Timer,
+     text: String,
+     progress: usize,
+     finished: bool,
+     update_timer: Timer,
 }
 
 impl DialogBox {
-    pub fn new(text: String, update_time: f32) -> Self {
-        DialogBox {
-            text,
-            update_timer: Timer::from_seconds(update_time, true),
-            finished: false,
-            progress: 0,
-        }
-    }
+     pub fn new(text: String, update_time: f32) -> Self {
+          DialogBox {
+               text,
+               update_timer: Timer::from_seconds(update_time, true),
+               finished: false,
+               progress: 0,
+          }
+     }
 }
 
 #[derive(Component)]
 pub struct DialogBoxText;
 #[derive(Component)]
 pub struct Scroll {
-    current_frame: usize,
-    reverse: bool,
+     current_frame: usize,
+     reverse: bool,
 }
 #[derive(Component, Deref, DerefMut)]
 pub struct ScrollTimer(Timer);
@@ -79,68 +79,68 @@ pub struct CreateDialogBoxEvent {
 pub struct CloseDialogBoxEvent;
 
 pub struct DialogBoxResources {
-    text_font: Handle<Font>,
-    appartements: Handle<Image>,
-    stained_glass_panels: Handle<Image>,
-    background: Handle<Image>,
-    _stained_glass_closed: Handle<Image>,
-    stained_glass_opened: Handle<Image>,
-    _stained_glass_bars: Handle<Image>,
-    chandelier: Handle<Image>,
-    scroll_animation: Vec<Handle<Image>>,
+     text_font: Handle<Font>,
+     appartements: Handle<Image>,
+     stained_glass_panels: Handle<Image>,
+     background: Handle<Image>,
+     _stained_glass_closed: Handle<Image>,
+     stained_glass_opened: Handle<Image>,
+     _stained_glass_bars: Handle<Image>,
+     chandelier: Handle<Image>,
+     scroll_animation: Vec<Handle<Image>>,
 }
 
 pub fn load_textures(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    // mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+     mut commands: Commands,
+     asset_server: Res<AssetServer>,
+     // mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    // let scroll_texture = asset_server.load("textures/hud/scroll_animation.png");
-    // let scroll_atlas = TextureAtlas::from_grid(scroll_texture, SCROLL_SIZE.into(), 1, 45);
+     // let scroll_texture = asset_server.load("textures/hud/scroll_animation.png");
+     // let scroll_atlas = TextureAtlas::from_grid(scroll_texture, SCROLL_SIZE.into(), 1, 45);
 
-    let mut scroll_animation_frames = vec![];
-    for i in 0..SCROLL_ANIMATION_FRAMES_NUMBER {
-        scroll_animation_frames
-            .push(asset_server.load(&format!("textures/hud/scroll_animation/frame_{}.png", i)));
-    }
+     let mut scroll_animation_frames = vec![];
+     for i in 0..SCROLL_ANIMATION_FRAMES_NUMBER {
+          scroll_animation_frames
+               .push(asset_server.load(&format!("textures/hud/scroll_animation/frame_{}.png", i)));
+     }
 
-    commands.insert_resource(DialogBoxResources {
-        text_font: asset_server.load("fonts/dpcomic.ttf"),
-        appartements: asset_server.load("textures/hud/papier_paint.png"),
-        background: asset_server.load("textures/hud/dialog_background.png"),
-        scroll_animation: scroll_animation_frames,
-        chandelier: asset_server.load("textures/hud/chandelier.png"),
-        _stained_glass_closed: asset_server.load("textures/hud/stained_glass_closed.png"),
-        stained_glass_opened: asset_server.load("textures/hud/stained_glass_opened.png"),
-        _stained_glass_bars: asset_server.load("textures/hud/stained_glass_bars.png"),
-        stained_glass_panels: asset_server.load("textures/hud/stained_glass_panels.png"),
-    });
+     commands.insert_resource(DialogBoxResources {
+          text_font: asset_server.load("fonts/dpcomic.ttf"),
+          appartements: asset_server.load("textures/hud/papier_paint.png"),
+          background: asset_server.load("textures/hud/dialog_background.png"),
+          scroll_animation: scroll_animation_frames,
+          chandelier: asset_server.load("textures/hud/chandelier.png"),
+          _stained_glass_closed: asset_server.load("textures/hud/stained_glass_closed.png"),
+          stained_glass_opened: asset_server.load("textures/hud/stained_glass_opened.png"),
+          _stained_glass_bars: asset_server.load("textures/hud/stained_glass_bars.png"),
+          stained_glass_panels: asset_server.load("textures/hud/stained_glass_panels.png"),
+     });
 }
 
 // TODO PB Spamming the ui key 'o' throws an error
 pub fn create_dialog_box_on_key_press(
-    mut create_dialog_box_event: EventWriter<CreateDialogBoxEvent>,
-    mut close_dialog_box_event: EventWriter<CloseDialogBoxEvent>,
+     mut create_dialog_box_event: EventWriter<CreateDialogBoxEvent>,
+     mut close_dialog_box_event: EventWriter<CloseDialogBoxEvent>,
 
-    mut ev_combat_exit: EventWriter<CombatExitEvent>,
+     mut ev_combat_exit: EventWriter<CombatExitEvent>,
 
-    query: Query<(Entity, &Animator<Style>, &Style), With<DialogPanel>>,
-    keyboard_input: Res<Input<KeyCode>>,
+     query: Query<(Entity, &Animator<Style>, &Style), With<DialogPanel>>,
+     keyboard_input: Res<Input<KeyCode>>,
 ) {
-    if keyboard_input.just_pressed(KeyCode::O) {
-        if let Ok((_entity, animator, _style)) = query.get_single() {
-            if animator.tweenable().unwrap().progress() >= 1.0 {
-                close_dialog_box_event.send(CloseDialogBoxEvent);
+     if keyboard_input.just_pressed(KeyCode::O) {
+          if let Ok((_entity, animator, _style)) = query.get_single() {
+               if animator.tweenable().unwrap().progress() >= 1.0 {
+                    close_dialog_box_event.send(CloseDialogBoxEvent);
 
-                ev_combat_exit.send(CombatExitEvent);
-            }
-        } else {
-            info!("here second");
-            create_dialog_box_event.send(CreateDialogBoxEvent {
-                dialog: "Bonjour Florian. \nComment vas-tu ? \nJ'ai faim.".to_owned(),
-            });
-        }
-    }
+                    ev_combat_exit.send(CombatExitEvent);
+               }
+          } else {
+               info!("here second");
+               create_dialog_box_event.send(CreateDialogBoxEvent {
+                    dialog: "Bonjour Florian. \nComment vas-tu ? \nJ'ai faim.".to_owned(),
+               });
+          }
+     }
 }
 
 /// Handle the CombatEvent
@@ -150,27 +150,27 @@ pub fn create_dialog_box_on_key_press(
 /// read CombatExitEvent
 ///     close any open ui
 pub fn create_dialog_box_on_combat_event(
-    mut create_dialog_box_event: EventWriter<CreateDialogBoxEvent>,
-    // mut close_dialog_box_event: EventWriter<CloseDialogBoxEvent>,
-    query: Query<(Entity, &Animator<Style>, &Style), With<DialogPanel>>,
-    mut ev_combat: EventReader<CombatEvent>,
-    // mut ev_combat_exit: EventReader<CombatExitEvent>,
-    // with dialog
-    npc_query: Query<(Entity, &Dialog), With<NPC>>,
+     mut create_dialog_box_event: EventWriter<CreateDialogBoxEvent>,
+     // mut close_dialog_box_event: EventWriter<CloseDialogBoxEvent>,
+     query: Query<(Entity, &Animator<Style>, &Style), With<DialogPanel>>,
+     mut ev_combat: EventReader<CombatEvent>,
+     // mut ev_combat_exit: EventReader<CombatExitEvent>,
+     // with dialog
+     npc_query: Query<(Entity, &Dialog), With<NPC>>,
 ) {
-    // order : exit combat UI
-    // for _ev in ev_combat_exit.iter()
-    // {
-    //     // and UI is open
-    //     if let Ok((_entity, animator, _style)) = query.get_single()
-    //     {
-    //         if animator.tweenable().unwrap().progress() >= 1.0 {
-    //             close_dialog_box_event.send(CloseDialogBoxEvent);
-    //         }
-    //     }
-    // }
+     // order : exit combat UI
+     // for _ev in ev_combat_exit.iter()
+     // {
+     //     // and UI is open
+     //     if let Ok((_entity, animator, _style)) = query.get_single()
+     //     {
+     //         if animator.tweenable().unwrap().progress() >= 1.0 {
+     //             close_dialog_box_event.send(CloseDialogBoxEvent);
+     //         }
+     //     }
+     // }
 
-    // TODO separate into two function
+     // TODO separate into two function
 
      for ev in ev_combat.iter() {
           // if already open go to combat tab
@@ -232,139 +232,139 @@ pub fn create_dialog_box_on_combat_event(
                     }
                }
 
-                Err(_e) => {
+                    Err(_e) => {
                     create_dialog_box_event.send(CreateDialogBoxEvent {
-                        dialog: "TALK \nFIGHT".to_owned(),
+                         dialog: "TALK \nFIGHT".to_owned(),
                     });
-                }
-            }
-        }
-    }
+                    }
+               }
+          }
+     }
 }
 
 pub fn close_dialog_box(
-    mut commands: Commands,
-    mut close_dialog_box_events: EventReader<CloseDialogBoxEvent>,
-    mut query: Query<(Entity, &mut Animator<Style>, &Style), With<DialogPanel>>,
+     mut commands: Commands,
+     mut close_dialog_box_events: EventReader<CloseDialogBoxEvent>,
+     mut query: Query<(Entity, &mut Animator<Style>, &Style), With<DialogPanel>>,
 ) {
-    for CloseDialogBoxEvent in close_dialog_box_events.iter() {
-        info!("close dialog event");
-        if let Ok((entity, mut _animator, style)) = query.get_single_mut() {
-            let dialog_box_tween = Tween::new(
-                EaseFunction::QuadraticIn,
-                TweeningType::Once,
-                Duration::from_millis(DIALOG_BOX_ANIMATION_TIME_MS),
-                UiPositionLens {
+     for CloseDialogBoxEvent in close_dialog_box_events.iter() {
+          info!("close dialog event");
+          if let Ok((entity, mut _animator, style)) = query.get_single_mut() {
+               let dialog_box_tween = Tween::new(
+                    EaseFunction::QuadraticIn,
+                    TweeningType::Once,
+                    Duration::from_millis(DIALOG_BOX_ANIMATION_TIME_MS),
+                    UiPositionLens {
                     start: style.position,
                     end: UiRect {
-                        left: Val::Auto,
-                        top: Val::Px(0.0),
-                        right: Val::Px(DIALOG_BOX_ANIMATION_OFFSET),
-                        bottom: Val::Px(0.0),
+                         left: Val::Auto,
+                         top: Val::Px(0.0),
+                         right: Val::Px(DIALOG_BOX_ANIMATION_OFFSET),
+                         bottom: Val::Px(0.0),
                     },
-                },
-            )
-            .with_completed_event(0);
+                    },
+               )
+               .with_completed_event(0);
 
-            commands
-                .entity(entity)
-                .remove::<Animator<Style>>()
-                .insert(Animator::new(dialog_box_tween));
-        }
-    }
+               commands
+                    .entity(entity)
+                    .remove::<Animator<Style>>()
+                    .insert(Animator::new(dialog_box_tween));
+          }
+     }
 }
 
 pub fn despawn_dialog_box(
-    mut commands: Commands,
-    mut completed_event: EventReader<TweenCompleted>,
+     mut commands: Commands,
+     mut completed_event: EventReader<TweenCompleted>,
 ) {
-    for TweenCompleted { entity, user_data } in completed_event.iter() {
-        if *user_data == 0 {
-            commands.entity(*entity).despawn_recursive();
-        }
-    }
+     for TweenCompleted { entity, user_data } in completed_event.iter() {
+          if *user_data == 0 {
+               commands.entity(*entity).despawn_recursive();
+          }
+     }
 }
 
 pub fn create_dialog_box(
-    mut create_dialog_box_events: EventReader<CreateDialogBoxEvent>,
-    mut commands: Commands,
-    mut _meshes: ResMut<Assets<Mesh>>,
-    _texture_atlases: Res<Assets<TextureAtlas>>,
-    dialog_box_resources: Res<DialogBoxResources>,
-    asset_server: Res<AssetServer>,
+     mut create_dialog_box_events: EventReader<CreateDialogBoxEvent>,
+     mut commands: Commands,
+     mut _meshes: ResMut<Assets<Mesh>>,
+     _texture_atlases: Res<Assets<TextureAtlas>>,
+     dialog_box_resources: Res<DialogBoxResources>,
+     asset_server: Res<AssetServer>,
 ) {
-    for CreateDialogBoxEvent { dialog } in create_dialog_box_events.iter() {
-        info!("open dialog event");
-        let dialog_box_tween = Tween::new(
-            EaseFunction::QuadraticOut,
-            TweeningType::Once,
-            Duration::from_millis(DIALOG_BOX_ANIMATION_TIME_MS),
-            UiPositionLens {
-                start: UiRect {
+     for CreateDialogBoxEvent { dialog } in create_dialog_box_events.iter() {
+          info!("open dialog event");
+          let dialog_box_tween = Tween::new(
+               EaseFunction::QuadraticOut,
+               TweeningType::Once,
+               Duration::from_millis(DIALOG_BOX_ANIMATION_TIME_MS),
+               UiPositionLens {
+                    start: UiRect {
                     left: Val::Auto,
                     top: Val::Px(0.0),
                     right: Val::Px(DIALOG_BOX_ANIMATION_OFFSET),
                     bottom: Val::Px(0.0),
-                },
-                end: UiRect {
+                    },
+                    end: UiRect {
                     left: Val::Auto,
                     top: Val::Px(0.0),
                     right: Val::Px(0.0),
                     bottom: Val::Px(0.0),
-                },
-            },
-        );
+                    },
+               },
+          );
 
-        let panels_tween = Tween::new(
-            EaseMethod::Linear,
-            TweeningType::Once,
-            Duration::from_millis(1000),
-            UiPositionLens {
-                start: UiRect {
+          let panels_tween = Tween::new(
+               EaseMethod::Linear,
+               TweeningType::Once,
+               Duration::from_millis(1000),
+               UiPositionLens {
+                    start: UiRect {
                     top: Val::Px(0.0),
                     ..UiRect::default()
-                },
-                end: UiRect {
+                    },
+                    end: UiRect {
                     top: Val::Px(-160.0),
                     ..UiRect::default()
-                },
-            },
-        );
+                    },
+               },
+          );
 
-        commands
-            .spawn_bundle(ImageBundle {
-                image: dialog_box_resources.appartements.clone().into(),
-                style: Style {
+          commands
+               .spawn_bundle(ImageBundle {
+                    image: dialog_box_resources.appartements.clone().into(),
+                    style: Style {
                     display: Display::Flex,
                     flex_direction: FlexDirection::Column,
                     align_items: AlignItems::Center,
                     justify_content: JustifyContent::Center,
                     position_type: PositionType::Relative,
                     position: UiRect {
-                        top: Val::Px(0.0),
-                        left: Val::Auto,
-                        right: Val::Px(DIALOG_BOX_ANIMATION_OFFSET),
-                        bottom: Val::Px(0.0),
+                         top: Val::Px(0.0),
+                         left: Val::Auto,
+                         right: Val::Px(DIALOG_BOX_ANIMATION_OFFSET),
+                         bottom: Val::Px(0.0),
                     },
                     margin: UiRect {
-                        left: Val::Auto,
-                        right: Val::Px(0.0),
-                        top: Val::Px(0.0),
-                        bottom: Val::Px(0.0),
+                         left: Val::Auto,
+                         right: Val::Px(0.0),
+                         top: Val::Px(0.0),
+                         bottom: Val::Px(0.0),
                     },
                     size: Size::new(Val::Auto, Val::Percent(100.0)),
                     aspect_ratio: Some(284.0 / 400.0),
                     ..Style::default()
-                },
-                ..ImageBundle::default()
-            })
-            .insert(Name::new("UI Wall"))
-            .with_children(|parent| {
-                let child_sprite_style = Style {
+                    },
+                    ..ImageBundle::default()
+               })
+               .insert(Name::new("UI Wall"))
+               .with_children(|parent| {
+                    let child_sprite_style = Style {
                     position_type: PositionType::Absolute,
                     size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                     ..Style::default()
-                };
+                    };
 
                // panels under the wall to prevent them from sticking out of the window after being lifted.
                parent
@@ -489,9 +489,9 @@ pub fn create_dialog_box(
 }
 
 pub fn update_dialog_box(
-    time: Res<Time>,
-    mut dialog_box_query: Query<(&mut DialogBox, &Children)>,
-    mut text_query: Query<&mut Text>,
+     time: Res<Time>,
+     mut dialog_box_query: Query<(&mut DialogBox, &Children)>,
+     mut text_query: Query<&mut Text>,
 ) {
      for (mut dialog_box, children) in dialog_box_query.iter_mut() {
           dialog_box.update_timer.tick(time.delta());
@@ -511,31 +511,31 @@ pub fn update_dialog_box(
 }
 
 pub fn animate_scroll(
-    time: Res<Time>,
-    // texture_atlases: Res<Assets<TextureAtlas>>,
-    dialog_box_resources: Res<DialogBoxResources>,
-    mut commands: Commands,
-    mut scroll_query: Query<(&mut UiImage, &mut Scroll, &mut ScrollTimer, Entity)>,
+     time: Res<Time>,
+     // texture_atlases: Res<Assets<TextureAtlas>>,
+     dialog_box_resources: Res<DialogBoxResources>,
+     mut commands: Commands,
+     mut scroll_query: Query<(&mut UiImage, &mut Scroll, &mut ScrollTimer, Entity)>,
 ) {
-    for (mut image, mut scroll, mut timer, entity) in scroll_query.iter_mut() {
-        timer.tick(time.delta());
+     for (mut image, mut scroll, mut timer, entity) in scroll_query.iter_mut() {
+          timer.tick(time.delta());
 
-        if timer.finished() {
-            if scroll.reverse {
-                scroll.current_frame -= 1;
+          if timer.finished() {
+               if scroll.reverse {
+                    scroll.current_frame -= 1;
 
-                if scroll.current_frame == 0 {
+                    if scroll.current_frame == 0 {
                     commands.entity(entity).remove::<ScrollTimer>();
-                }
-            } else {
-                scroll.current_frame += 1;
+                    }
+               } else {
+                    scroll.current_frame += 1;
 
-                if scroll.current_frame >= SCROLL_ANIMATION_FRAMES_NUMBER - 1 {
+                    if scroll.current_frame >= SCROLL_ANIMATION_FRAMES_NUMBER - 1 {
                     commands.entity(entity).remove::<ScrollTimer>();
-                }
-            }
+                    }
+               }
 
-            image.0 = dialog_box_resources.scroll_animation[scroll.current_frame].clone();
-        }
-    }
+               image.0 = dialog_box_resources.scroll_animation[scroll.current_frame].clone();
+          }
+     }
 }
