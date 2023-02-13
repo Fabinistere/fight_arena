897ec7291f6604dfa55312a59f18c119817bd177
Wabtey <samurai.banzai@laposte.net>
2023-02-13 23:16:48 +0100

    add webassembly support
    can host a local server with the game in it

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,10 @@ pub enum GameState {
 }
 
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+    
     let height = 720.0;
 
     let mut app = App::new();
